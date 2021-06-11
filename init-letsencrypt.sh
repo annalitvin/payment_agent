39ffa13d5361d4068e1f4ef1a49d3fb9fce7964b
annalitvin <litvin_any@ukr.net>
2021-06-11 08:52:17 +0300

    Added support ssl serf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(3.19.213.103:85)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
